106f45dadeecf6bff2530f72ee4447ae9a2c4083
RafaPerez05 <126112322+RafaPerez05@users.noreply.github.com>
2023-05-08 23:17:45 -0300

    first commit

---
+++ main.php
@@ -0,0 +1,17 @@
+<?php
+
+require "produto.php";
+//Instaciando objeto carro 1 
+$produto1 = new Produto ("celular", "iphone", "preto",200);
+
+$produto1->atualizar(10);
+$produto1->nome();
+
+
+
+
+
+
+
+
+?>
